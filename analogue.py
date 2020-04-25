7655fc99945e9ebc43ed6e32bcfcfd84f1969fc3
Hugh A. Miles <hugh@Hughs-MacBook-Pro.local>
2020-04-24 21:53:52 -0400

    move things around

--- analogue.py
+++ analogue.py
@@ -5,9 +5,9 @@ API_KEY = 'your-pinboard-api-key'
 
 
 def main(wf):
-    url = 'https://api.pinboard.in/v1/posts/recent'
-    params = dict(auth_token=API_KEY, count=20, format='json')
-    r = web.get(url, params)
+    url = 'https://pysrv.now.sh/'
+    # params = dict(auth_token=API_KEY, count=20, format='json')
+    r = web.get(url)
 
     # throw an error if request failed
     # Workflow will catch this and show it to the user
@@ -15,18 +15,16 @@ def main(wf):
 
     # Parse the JSON returned by pinboard and extract the posts
     result = r.json()
-    posts = result['posts']
 
     # Loop through the returned posts and add an item for each to
     # the list of results for Alfred
-    for post in posts:
-        wf.add_item(title=post['description'],
-                    subtitle=post['href'],
+    for planet in result['hello']:
+        wf.add_item(title=planet,
+                    subtitle=planet,
                     icon=ICON_WEB)
 
     # Send the results to Alfred as XML
     wf.send_feedback()
-
 
 if __name__ == u"__main__":
     wf = Workflow()
