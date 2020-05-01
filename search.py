36bbafb4fab9a56593ee50aa0ce9cbf02234a0dc
J03L R0J0 <rojo@analogue.app>
2020-04-30 19:50:54 -0700

    ⚡️ Added logging + attempt thumbnails

--- search.py
+++ search.py
@@ -1,7 +1,15 @@
 import sys
-from workflow import Workflow, ICON_WEB, web
-from lib import search_analogue
+from workflow import Workflow, ICON_WARNING
+from workflow.background import run_in_background, is_running
 
+from lib import search_analogue
+from thumbnails import Thumbs
+
+# import logging
+from datetime import date, datetime
+import time
+
+log = None
 
 def main(wf):
     if len(wf.args):
@@ -9,22 +17,42 @@ def main(wf):
     else:
         query = None
 
-    result = search_analogue(query=query)
+    log.debug('query : %r', query)
 
-    for hit in result:
-        # The user is tagged in the url query action
+    results = search_analogue(query=query)
+
+    log.debug('%d results.', len(results))
+
+    if not results:
+        wf.add_item('No results found for "%s".' % query,
+                    'Try a different query or add a link.')
+
+    thumbs = Thumbs(wf.datafile('thumbs'))
+    for hit in results:
         wf.add_item(
-            title=hit["title"],
-            subtitle=hit["excerpt"],
-            valid=True,
-            arg="https://analogue.app/{}/{}/".format(hit["form_slug"],hit["slug"]),
-            icon=hit["image_url"],
+            title = hit['title'],
+            subtitle = hit['excerpt'],
+            valid = True,
+            arg = 'https://analogue.app/{}/{}/'.format(hit['form_slug'],hit['slug']),
+            icon = thumbs.thumbnail(hit.get('image_url')),
+            icontype = None
         )
 
     # Send the results to Alfred as XML
     wf.send_feedback()
 
+    # Handle thumbnail queue
+    thumbs.save_queue()
+    if thumbs.has_queue:
+        if not is_running('generate_thumbnails'):
+            run_in_background('generate_thumbnails',
+                              ['/usr/bin/python',
+                               wf.workflowfile('thumbnails.py')])
+
+    return 0
+
 
 if __name__ == u"__main__":
     wf = Workflow()
+    log = wf.logger
     sys.exit(wf.run(main))
