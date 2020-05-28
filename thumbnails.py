bbdc8ef10a4914061ed41786964787e972f7ff06
Hugh A. Miles <hugh@Hughs-MacBook-Pro.local>
2020-05-27 22:30:40 -0400

    fix small error

--- thumbnails.py
+++ thumbnails.py
@@ -36,7 +36,7 @@ class Thumbs(object):
             cachedir (str): Path to directory to save thumbnails in.
         """
         self._cachedir = os.path.abspath(cachedir)
-        self._queue_path = os.path.join(self._cachedir, 'thumbnails.txt')
+        self._queue_path = os.path.join(self._cachedir, "thumbnails.txt")
         self._queue = []
 
         try:
@@ -63,13 +63,13 @@ class Thumbs(object):
             str: Path to thumbnail.
         """
         if isinstance(img_path, unicode):
-            img_path = img_path.encode('utf-8')
+            img_path = img_path.encode("utf-8")
         elif not isinstance(img_path, str):
             img_path = str(img_path)
 
         h = hashlib.md5(img_path).hexdigest()
         dirpath = os.path.join(self.cachedir, h[:2], h[2:4])
-        thumb_path = os.path.join(dirpath, u'{}.png'.format(h))
+        thumb_path = os.path.join(dirpath, "{}.png".format(h))
         return thumb_path
 
     def thumbnail(self, img_path):
@@ -85,9 +85,9 @@ class Thumbs(object):
 
         if os.path.exists(thumb_path):
             return thumb_path
-        else:
-            self.queue_thumbnail(img_path)
-            return None
+
+        self.queue_thumbnail(img_path)
+        return None
 
     def queue_thumbnail(self, img_path):
         """Add ``img_path`` to queue for later thumbnail generation.
@@ -105,14 +105,14 @@ class Thumbs(object):
         text = []
         for p in self._queue:
             if isinstance(p, unicode):
-                p = p.encode('utf-8')
+                p = p.encode("utf-8")
             text.append(p)
-            log.debug('Queued for thumbnail generation : %r', p)
+            log.debug("Queued for thumbnail generation : %r", p)
 
-        text = b'\n'.join(text)
+        text = b"\n".join(text)
         with LockFile(self._queue_path):
-            with atomic_writer(self._queue_path, 'ab') as fp:
-                fp.write(b'{}\n'.format(text))
+            with atomic_writer(self._queue_path, "ab") as fp:
+                fp.write(b"{}\n".format(text))
 
         self._queue = []
 
@@ -123,13 +123,14 @@ class Thumbs(object):
         Returns:
             bool: ``True`` if there's a queue.
         """
-        return (os.path.exists(self._queue_path) and
-                os.path.getsize(self._queue_path) > 0)
+        return (
+            os.path.exists(self._queue_path) and os.path.getsize(self._queue_path) > 0
+        )
 
     def process_queue(self):
         """Generate thumbnails for queued files."""
         if not self.has_queue:
-            log.debug('Thumbnail queue empty.')
+            log.debug("Thumbnail queue empty.")
             return
 
         queue = []
@@ -140,12 +141,12 @@ class Thumbs(object):
                     if not line:
                         continue
                     queue.append(line)
-            with atomic_writer(self._queue_path, 'wb') as fp:
-                fp.write('')
+            with atomic_writer(self._queue_path, "wb") as fp:
+                fp.write("")
 
         succeeded = True
         for i, img_path in enumerate(queue):
-            log.debug('Generating thumbnail %d/%d ...', i + 1, len(queue))
+            log.debug("Generating thumbnail %d/%d ...", i + 1, len(queue))
             if not self.generate_thumbnail(img_path):
                 succeeded = False
 
@@ -169,7 +170,7 @@ class Thumbs(object):
 
         urllib.urlretrieve(img_path, thumb_path)
 
-        log.debug('Wrote thumbnail for `%s` to `%s`.', img_path, thumb_path)
+        log.debug("Wrote thumbnail for `%s` to `%s`.", img_path, thumb_path)
 
         return True
 
@@ -180,10 +181,11 @@ def main(wf):
     Args:
         wf (Workflow): Current workflow instance.
     """
-    t = Thumbs(wf.datafile('thumbs'))
+    t = Thumbs(wf.datafile("thumbs"))
     t.process_queue()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     wf = Workflow()
     log = wf.logger
     sys.exit(wf.run(main))
